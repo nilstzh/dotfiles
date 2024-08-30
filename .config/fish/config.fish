9d4d94db0ab4e9b5294d5b08d5eb5d32fe5b4380
Nils Truzhenko <nils.truzhenko@brevo.com>
2024-08-30 18:13:40 +0300

    Update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status --is-interactive
     # nothing here yet
 end
 
-source /usr/local/opt/asdf/libexec/asdf.fish
+source (brew --prefix asdf)/libexec/asdf.fish
 
 alias branch="git for-each-ref --sort=-committerdate refs/heads --format='%(authordate:short) %(color:red)%(objectname:short) %(color:yellow)%(refname:short)%(color:reset) (%(color:green)%(committerdate:relative)%(color:reset))' --count=10"
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
